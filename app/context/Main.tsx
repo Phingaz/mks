40913ce49b69098d62fe214b8dce59024f6341e5
Prosper Noya <piinoya@gmail.com>
2024-04-15 22:28:57 -0300

    updates

--- app/context/Main.tsx
+++ app/context/Main.tsx
@@ -30,7 +30,6 @@ export function MainCtxProvider({ children }: React.PropsWithChildren<{}>) {
     const response = await fetch(
       "https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=name&orderBy=DESC"
     );
-    await new Promise((resolve) => setTimeout(resolve, 1000000));
     const data = await response.json();
     return data;
   };
